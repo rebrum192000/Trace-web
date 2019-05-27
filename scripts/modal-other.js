85792273976b51169b226ec7a1ea30b975986335
Konstantin <rebrum19200@gmail.com>
2019-05-27 13:33:15 +0300

    Add files via upload

---
+++ scripts/modal-other.js
@@ -0,0 +1,49 @@
+var modalForm = document.querySelector('.modal__form');
+var mOpenBtn1 = document.querySelector('.jsButton1');
+var mOpenBtn2 = document.querySelector('.jsButton2');
+var mOpenBtn3 = document.querySelector('.jsButton3');
+var mOpenBtn4 = document.querySelector('.jsButton4');
+var mOpenBtn5 = document.querySelector('.jsButton5');
+var mOpenBtn6 = document.querySelector('.jsButton6');
+var mOpenBtn7 = document.querySelector('.jsButton7');
+var mOpenBtn8 = document.querySelector('.jsButton8');
+var mOpenBtn9 = document.querySelector('.jsButton9');
+var mOpenBtn10 = document.querySelector('.jsButton10');
+var mCloseBtn = document.querySelector('.modal__form--close-btn');
+
+
+	mOpenBtn1.onclick = function() {
+		modalForm.setAttribute("style", "display:block");
+	};
+	mOpenBtn2.onclick = function() {
+		modalForm.setAttribute("style", "display:block");
+	};
+	mOpenBtn3.onclick = function() {
+		modalForm.setAttribute("style", "display:block");
+	};
+	mOpenBtn4.onclick = function() {
+		modalForm.setAttribute("style", "display:block");
+	};
+	mOpenBtn5.onclick = function() {
+		modalForm.setAttribute("style", "display:block");
+	};
+	mOpenBtn6.onclick = function() {
+		modalForm.setAttribute("style", "display:block");
+	};
+	mOpenBtn7.onclick = function() {
+		modalForm.setAttribute("style", "display:block");
+	};
+	mOpenBtn8.onclick = function() {
+		modalForm.setAttribute("style", "display:block");
+	};
+	mOpenBtn9.onclick = function() {
+		modalForm.setAttribute("style", "display:block");
+	};
+	mOpenBtn10.onclick = function() {
+		modalForm.setAttribute("style", "display:block");
+	};
+
+
+	mCloseBtn.onclick = function() {
+		modalForm.setAttribute("style", "display:none");
+	};
